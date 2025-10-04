d8d8a632c7ad9766e54c4840604a4acf49b634b2
NITHIN KUMAR S <156948837+Nithinkumar1123@users.noreply.github.com>
2025-10-05 03:02:55 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,6 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:5000";
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || "https://alertmed.onrender.com";
 export const socket = io(SOCKET_URL, { autoConnect: false }); // connect manually
+
