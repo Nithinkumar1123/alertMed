c1f11ba4375cfe2f74731855ebcfbf5552d60e68
NITHIN KUMAR S <156948837+Nithinkumar1123@users.noreply.github.com>
2025-10-05 02:18:26 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://alertmed.onrender.com/api",
 });
 
 // attach token if present
@@ -13,3 +13,4 @@ API.interceptors.request.use((config) => {
 });
 
 export default API;
+
